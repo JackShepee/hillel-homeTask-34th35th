797833d290664d5107d7e97b7e6fbd840b52092f
VSalamakha <salamakhaviktor@gmail.com>
2023-09-18 22:53:21 +0300

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -11,18 +11,11 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(ts|tsx|js|jsx)$/,
+                test: /\.(ts|tsx)$/,
                 exclude: /node_modules/,
                 use: [
                     {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: [
-                                '@babel/preset-env',
-                                '@babel/preset-react',
-                                '@babel/preset-typescript',
-                            ],
-                        },
+                        loader: 'ts-loader',
                     },
                 ],
             },
